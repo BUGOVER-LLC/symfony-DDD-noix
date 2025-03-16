85faa53775f3a2501da5db06266605e0b6dae8f8
BUGOVER <manukyan.artak.21@gmail.com>
2025-03-17 01:59:43 +0400

    Elk configuration

--- src/Acl/Infrastructure/Controller/GetRolesController.php
+++ src/Acl/Infrastructure/Controller/GetRolesController.php
@@ -4,17 +4,21 @@ declare(strict_types=1);
 
 namespace App\Acl\Infrastructure\Controller;
 
-use App\Acl\Application\UseCase\Query\FindRoles\FindAllRoleByWorkspaceQuery;
+use Monolog\Attribute\WithMonologChannel;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('IS_AUTHENTICATED_FULLY')]
 #[Route(path: '/api/v1/roles', methods: ['GET', 'HEAD'])]
+#[WithMonologChannel('app')]
 class GetRolesController extends AbstractController
 {
-    public function __invoke()
+    public function __invoke(LoggerInterface $logger)
     {
-        $roles = (new FindAllRoleByWorkspaceQuery($workspace));
+        $logger->error('fewfewfhew ewfg yewgfyg yewfyfew');
+
+        return $this->json([]);
     }
 }
