3ed918394958d4ce2dc71b6438bbdaf6fd393cc4
BUGOVER <manukyan.artak.21@gmail.com>
2025-03-18 09:56:40 +0400

    Start new task and wait until it is done.

---
+++ src/Workspaces/Infrastructure/EventListener/Doctrine/WorkspaceWorkerDeleted.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Workspaces\Infrastructure\EventListener\Doctrine;
+
+use App\Workspaces\Domain\Entity\Worker;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Event\PostRemoveEventArgs;
+use Doctrine\ORM\Events;
+
+#[AsEntityListener(event: Events::postRemove, method: Events::postRemove, entity: Worker::class)]
+class WorkspaceWorkerDeleted
+{
+    public function __construct()
+    {
+    }
+
+    public function postRemove(PostRemoveEventArgs $args)
+    {
+    }
+}
