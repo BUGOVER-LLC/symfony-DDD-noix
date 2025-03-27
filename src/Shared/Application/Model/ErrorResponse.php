76396bfc84736f4bd10ef82936c4b851d4e760bf
BUGOVER <manukyan.artak.21@gmail.com>
2025-03-28 02:47:21 +0400

    adding customer request DTO resolver
    exception ...

---
+++ src/Shared/Application/Model/ErrorResponse.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Made for YouTube channel https://www.youtube.com/@eazy-dev
+ */
+
+namespace App\Shared\Application\Model;
+
+use Nelmio\ApiDocBundle\Model\Model;
+use OpenApi\Attributes as OA;
+use Symfony\Component\PropertyInfo\Type;
+
+class ErrorResponse
+{
+    public function __construct(private readonly string $message, private readonly mixed $details = null)
+    {
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    #[OA\Property(type: 'object', nullable: true, oneOf: [
+        new OA\Schema(ref: new Model(type: new Type(ErrorDebugDetails::class))),
+        new OA\Schema(ref: new Model(type: new Type(ErrorValidationDetails::class))),
+    ])]
+    public function getDetails(): mixed
+    {
+        return $this->details;
+    }
+}
--- src/User/Infrastructure/Exception/UserAlreadyHaveInvitationException.php
+++ src/User/Infrastructure/Exception/UserAlreadyHaveInvitationException.php
@@ -6,11 +6,13 @@ namespace App\User\Infrastructure\Exception;
 
 use RuntimeException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\WithHttpStatus;
 
+#[WithHttpStatus(Response::HTTP_FAILED_DEPENDENCY, headers: ['x-header' => 'Not found'])]
 class UserAlreadyHaveInvitationException extends RuntimeException
 {
     public function __construct()
     {
-        parent::__construct('Already have an invitation.', Response::HTTP_FAILED_DEPENDENCY);
+        parent::__construct('Already have an invitation.');
     }
 }
