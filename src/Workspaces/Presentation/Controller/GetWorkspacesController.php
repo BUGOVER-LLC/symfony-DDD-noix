b3a110190de48756e786443ff02348b7b3591a4a
BUGOVER <manukyan.artak.21@gmail.com>
2025-04-16 19:57:52 +0400

    Work on xdebug enables but not success
    In progress User creates a command
    and adds in workers' table if none exists or adds a workspace.
    
    Set current workspace query interactor.
    
    Move roles domain.
    
    In progress oauth
    
    In progress Media and Storage domains

--- src/Workspaces/Presentation/Controller/GetWorkspacesController.php
+++ src/Workspaces/Presentation/Controller/GetWorkspacesController.php
@@ -5,15 +5,24 @@ declare(strict_types=1);
 namespace App\Workspaces\Presentation\Controller;
 
 use App\Shared\Application\Attribute\RequestBody;
+use App\Shared\Application\Model\ErrorResponse;
 use App\Workspaces\Infrastructure\RequestDTO\GetWorkspaceDTO;
+use Nelmio\ApiDocBundle\Attribute\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use OpenApi\Attributes as OA;
 
 #[IsGranted(attribute: 'IS_AUTHENTICATED_FULLY', statusCode: 401)]
 #[Route(path: '/api/v1/workspaces', methods: ['GET'])]
+#[OA\RequestBody]
+#[OA\Response(response: 409, description: 'Book category already exists', attachables: [
+    new Model(
+        type: ErrorResponse::class,
+    ),
+])]
 class GetWorkspacesController extends AbstractController
 {
     public function __construct(private readonly Security $security)
@@ -22,8 +31,6 @@ class GetWorkspacesController extends AbstractController
 
     public function __invoke(#[RequestBody] GetWorkspaceDTO $workspaceDTO): JsonResponse
     {
-
-
         return $this->json([]);
     }
 }
